1f081edc59a5190fdd77d029e0123c3bfb59c8d4
Veronika Vokounová <ver.cik@seznam.cz>
2023-01-02 20:30:53 +0100

    aktualizace

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -10,13 +10,16 @@ import Energy from "../Energy";
 const Dashboard = ({data}) => {
     return (
         <main className="dashboard">
+          <div className="dashboard__left">
             <Lights lights={data.lights} />
+          </div>
+          <div className="dashboard__right">
             <Climate climate={data.climate} 
                       temperature={data.climate.temperature}
                       humidity={data.climate.humidity}/>
             <Blinds state={data.blinds} />
             <Energy energyComsumption={data.energyConsumption} />
-
+          </div>
         </main>
     )
 }
